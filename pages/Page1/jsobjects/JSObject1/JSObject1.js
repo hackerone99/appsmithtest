b79d92372c9f2ea314aff373520c898eb08c8e1e
Yong Heng Khor <khoryongheng@ecquaria.com>
2023-02-16 07:15:45 +0000

    test4

--- pages/Page1/jsobjects/JSObject1/JSObject1.js
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -2,11 +2,38 @@ export default {
 	myVar1: [],
 	myVar2: {},
 	myFun1: () => {
+		const asd = true;
 
-		return	"<h1>123</h1>"
+		return asd
 	},
-	myFun2: async () => {
-		//use async-await or promises
+	
+	 createWorkbook: async() => {
 
-	}
+   const workbook = new ExcelJS.Workbook();
+
+   workbook.creator = 'Tomato';
+   workbook.lastModifiedBy = 'Tomato';
+   workbook.created = new Date();
+   workbook.modified = new Date();
+   workbook.calcProperties.fullCalcOnLoad = true;
+
+   const worksheet = workbook.addWorksheet('Tomato page 1', {
+      properties: {tabColor: {argb: '#FF0000'}},
+      pageSetup: {paperSize: 9, orientation: 'landscape'}
+    });
+
+   worksheet.getCell('A1').value = 'Hello, World!';
+   worksheet.getCell('B1').value = 'What time is it?';
+   worksheet.getCell('A2').value = 7;
+   worksheet.getCell('B2').value = '12pm';
+
+   const buf = await workbook.xlsx.writeBuffer();
+   const blob = new Blob([buf], {type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'});
+   const url = URL.createObjectURL(blob);
+
+   await download(
+            url, 
+            "test.xls",
+        );
+    }
 }
